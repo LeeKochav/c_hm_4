dcd0462e0c607e0bfd3f2fc81bc66c025bad3701
LeeKochav <leekochav@gmail.com>
2020-01-08 16:36:25 +0200

    add comments

--- trie.c
+++ trie.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #define WORD_SIZE 30
 
+//consructor for Trie
 Trie* initTrie()
 {
     Trie* root=(Trie*)malloc(sizeof(Trie));
@@ -11,6 +12,7 @@ Trie* initTrie()
     return root;
 }
 
+//constructor for node Trie
 Node* newNode(char let)
 {
     Node* node=(Node*)malloc(sizeof(Node));
@@ -33,14 +35,14 @@ void add(Trie* root, char word[],int sizeWord)
         char c=word[i];
         if(curr->children[c-'a']==NULL)
         {
-            curr->children[c-'a']=newNode(c);
+            curr->children[c-'a']=newNode(c);//create new node in the trie with the word letter
         }
         
-        curr=curr->children[c-'a'];
+        curr=curr->children[c-'a']; //node with the word letter already exist 
         
     }
-    curr->wordEnd=TRUE;
-    curr->count++;
+    curr->wordEnd=TRUE; //reaced the last letter of the word, update this is the last letter
+    curr->count++; // word occured 
 }
 
 void print(Trie* root)
@@ -52,7 +54,7 @@ void print(Trie* root)
 
 void printR(Trie* root)
 {
-    char str[WORD_SIZE]={};
+    char str[WORD_SIZE]={}; // represnt the word in the tree that will be printed
 
     printNodeR(root->head,str,0);
 }
@@ -61,18 +63,18 @@ void printNode(Node* node, char str[], int index)
 {
     if(node!=NULL)
     {
-    if(node->wordEnd==TRUE)
+    if(node->wordEnd==TRUE) //reached last word, print word
     {
         str[index]='\0';
-        printf("%s \t %ld \n",str, node->count);
+        printf("%s \t %ld \n",str, node->count); // print word+ number of occurrences
     }
     
-    for(int i=0; i<NUM_LETTERS; i++)
+    for(int i=0; i<NUM_LETTERS; i++)// ascending order a-->z
     {
-        if(node->children[i]!=NULL)
+        if(node->children[i]!=NULL) 
         {
         str[index]=i+'a';
-        printNode(node->children[i],str,index+1);
+        printNode(node->children[i],str,index+1); //move to next letter
         }
     }
     }
@@ -83,15 +85,15 @@ void printNodeR(Node* node, char str[], int index)
     if(node!=NULL)
     {
     
-    for(int i=NUM_LETTERS-1; i>=0; i--)
+    for(int i=NUM_LETTERS-1; i>=0; i--) //descending order from z-->a 
     {
         if(node->children[i]!=NULL)
         {
         str[index]=i+'a';
-        printNodeR(node->children[i],str,index+1);
+        printNodeR(node->children[i],str,index+1); //move to next letter 
         }
     }
-    if(node->wordEnd==TRUE)
+    if(node->wordEnd==TRUE) //print when reached last word descending order
     {
         str[index]='\0';
         printf("%s \t %ld \n",str, node->count);
@@ -99,20 +101,21 @@ void printNodeR(Node* node, char str[], int index)
     }
 }
 
+//initialize array of chars= word, within input characters
 int getWord(char w[])
 {
     char c;
     int size=0;
-    while(scanf("%c",&c)==1&&size<WORD_SIZE)
+    while(scanf("%c",&c)==1&&size<WORD_SIZE) 
     {
     
-        if((c=='\n')||(c=='\t')||(c==' '))
+        if((c=='\n')||(c=='\t')||(c==' ')) //indicated that this is the end of the word
         {
             return size;
         }
         if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
         {
-        if(c>='A'&&c<='Z')
+        if(c>='A'&&c<='Z') //to lowercase
         {
             c=c+32;
         }
@@ -127,14 +130,14 @@ void trieFree(Trie* root)
 {
    if(root!=NULL)
    {
-     nodeFree(root->head);
+     nodeFree(root->head); 
    } 
-   free(root);
+   free(root); 
 }
 
 void nodeFree(Node* node)
 {
-    for(int i=NUM_LETTERS-1; i>=0; i--)
+    for(int i=NUM_LETTERS-1; i>=0; i--) //travese nodes in trie to release memory 
     {
         if(node->children[i]!=NULL)
         {
@@ -153,23 +156,23 @@ int main(int argc, char* argv[])
     char word [WORD_SIZE];
     Trie* t=initTrie();
     size=getWord(word);
-    while(size>0)
+    while(size>0) //size=0->empty word means end of input , trie does not contain empty words.
     {
         add(t,word,size);
         size=getWord(word);
     }
-    if(argc==1)
+    if(argc==1) // no operation in the execution program command 
     {
         print(t);
     }
-    else if(argc==2)
+    else if(argc==2) // operation appears in the execution program command 
     {
-        if(strcmp(argv[1],"r")==0)
+        if(strcmp(argv[1],"r")==0) //operation is valid if and only if operatopn = 'r'
         {
         printR(t);
         }
     }
-    trieFree(t);
+    trieFree(t); //free trie memory allocation 
     return 0;
     
 }
